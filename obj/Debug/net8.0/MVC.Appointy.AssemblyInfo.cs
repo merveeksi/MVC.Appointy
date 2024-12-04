ca63431dcf2ddaa0ab297c5f39a1845becdbe326
MERVE EKŞİ <merveeksii61@gmail.com>
2024-12-04 19:22:04 +0300

    asd

--- obj/Debug/net8.0/MVC.Appointy.AssemblyInfo.cs
+++ obj/Debug/net8.0/MVC.Appointy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC.Appointy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1992b38e18833e44136db87763eb62f208d247db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba7ac5695ca51ad69877b1ec6c67725ec938cf98")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC.Appointy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC.Appointy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
